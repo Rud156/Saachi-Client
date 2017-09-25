b9389b8d457734e453c695788b0a86782ae3b9e8
Rud156 <rudrangshu_1997@yahoo.com>
2017-09-25 23:03:30 +0530

    UI Updates

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
     <v-app light>
-        <v-snackbar
-            :top="true"
-            :right="true"
-            :success="snackBarType === 'success'"
-            :info="snackBarType === 'info'"
-            :warning="snackBarType === 'warning'"
-            :error="snackBarType === 'error'"
-            v-model="displaySnackBar"
-        >
+        <v-snackbar :top="true" :right="true" :success="snackBarType === 'success'" :info="snackBarType === 'info'" :warning="snackBarType === 'warning'"
+            :error="snackBarType === 'error'" v-model="displaySnackBar">
             {{ snackBarMessage }}
             <v-btn flat class="white--text" @click.stop="displaySnackBar = false">Close</v-btn>
         </v-snackbar>
         <v-toolbar light class="extra-margin-bottom">
-            <v-toolbar-title class="red--text">Logo/Title</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-toolbar-items>
-                <v-btn flat>
-                    About Us
-                </v-btn>
-            </v-toolbar-items>
+            <v-toolbar-title class="red--text">Saachi</v-toolbar-title>
+            <v-spacer></v-spacer>
         </v-toolbar>
         <transition name="slide-x-transition">
             <router-view @displayMessage="displayMessage"></router-view>
         </transition>
         <v-footer absolute style="background-color: #fafafa">
-        <v-spacer></v-spacer>
-        <div class="title" style="padding: 7px">Desgined with 💗 by: Team Saachi</div>
+            <v-spacer></v-spacer>
+            <div class="title" style="padding: 7px">Desgined with 💗 by: Team Saachi</div>
         </v-footer>
     </v-app>
 </template>
